7a19d9579ef0468fa39698ce96489f91d402054b
zometh <metzodiop45@gmail.com>
2024-12-17 14:41:52 +0000

    Modification du message sur preprod

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     //Fonction principale pour afficher le message
     function main(){
-        echo "Hello, Github!";
+        echo "Hello, Guys it's me!";
     }
     //Appel de la fonction principale
     main();
